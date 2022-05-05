be492879bf5fe4d2ed602e76c4fa527178d77e25
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2022-05-05 19:20:19 +0300

    util time coverter

---
+++ app/src/main/java/com/bariscanyilmaz/musicplayer/utils/TimeConverter.java
@@ -0,0 +1,13 @@
+package com.bariscanyilmaz.musicplayer.utils;
+
+import java.util.concurrent.TimeUnit;
+
+public class TimeConverter {
+
+    public static String convertToMMSS(String duration){
+        Long millis = Long.parseLong(duration);
+        return String.format("%02d:%02d",
+                TimeUnit.MILLISECONDS.toMinutes(millis) % TimeUnit.HOURS.toMinutes(1),
+                TimeUnit.MILLISECONDS.toSeconds(millis) % TimeUnit.MINUTES.toSeconds(1));
+    }
+}
